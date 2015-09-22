b6a08674781d2bbce3da6b5725ba6c9f1ffa10bc
Alon Waisman, Justin Pease, Matthew Hecker and Regis Boudinot <pair+mowalon+jpease1020+hoffsmh+selfup@gmail.com>
2015-09-21 19:37:49 -0600

    Create categories page
    Added route/controller/view for categories index. Modified Categories dropdown in navbar to link to All Categories. Test unable to find link.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
+  resources :categories, only: [:index]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
