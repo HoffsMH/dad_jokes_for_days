9c363fdcdd36dd86b343cfeca2a5a465cc7869e5
Alon Waisman, Justin Pease, Matthew Hecker and Regis Boudinot <pair+mowalon+jpease1020+hoffsmh+selfup@gmail.com>
2015-09-22 16:38:05 -0600

    initial commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,15 @@
 class Item < ActiveRecord::Base
   belongs_to :category
+
+  validates :name, :dao, presence: true
+
+  before_validation do
+    if name
+      self.dao = name.downcase.gsub(" ", "")
+    end
+  end
+
+  def to_param
+    dao
+  end
 end
