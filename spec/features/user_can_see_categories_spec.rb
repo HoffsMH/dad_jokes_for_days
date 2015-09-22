b2e9b9faf3e44c21fd37f33b4e60aae78f2f8258
Alon Waisman, Justin Pease, Matthew Hecker and Regis Boudinot <pair+mowalon+jpease1020+hoffsmh+selfup@gmail.com>
2015-09-22 09:46:35 -0600

    Add fixtures for testing.
    Now the tests pass.

--- spec/features/user_can_see_categories_spec.rb
+++ spec/features/user_can_see_categories_spec.rb
@@ -1,13 +1,19 @@
 require "rails_helper"
 
 feature "A guest" do
+  fixtures :categories
+
   it "can visit the categories page" do
+    visit "/"
     click_link "All Categories"
+
     expect(current_path).to eq(categories_path)
   end
 
   it "can see all categories on the categories page" do
+    visit "/"
     click_link "All Categories"
+
     expect(page).to have_content("Category A")
     expect(page).to have_content("Category B")
     expect(page).to have_content("Category C")
