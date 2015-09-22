f21a0a40c76e3de0297a80c27fa268e4db0e2a0d
Alon Waisman, Justin Pease, Matthew Hecker and Regis Boudinot <pair+mowalon+jpease1020+hoffsmh+selfup@gmail.com>
2015-09-21 19:25:19 -0600

    Create feature test

---
+++ spec/features/user_can_see_categories_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+feature "Categories page:" do
+  scenario "a guest" do
+    
+    it "can visit the categories page" do
+      click_link "All Categories"
+      expect(current_path).to eq(categories_path)
+    end
+
+    it "can see all categories on the categories page" do
+      click_link "All Categories"
+      expect(page).to have_content("Category A")
+      expect(page).to have_content("Category B")
+      expect(page).to have_content("Category C")
+    end
+
+  end
+end
