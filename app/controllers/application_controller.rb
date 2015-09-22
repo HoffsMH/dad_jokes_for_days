f354295489098806c425e6228ef03ed0bbcc4cf6
Alon Waisman, Justin Pease, Matthew Hecker and Regis Boudinot <pair+mowalon+jpease1020+hoffsmh+selfup@gmail.com>
2015-09-22 12:58:58 -0600

    started working on views for lising categories correctly
    added some test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :load_categories
+
+  def load_categories
+    @categories = Categories.all 
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'welcome#index'
 
-  resources :categories, only: [:index]
+  resources :categories, only: [:index, :show]
   resources :items
 
   # The priority is based upon order of creation: first created -> highest priority.
