bf4516ee746f159084f237cc8d266858d8d12ec8
Alon Waisman, Justin Pease, Matthew Hecker and Regis Boudinot <pair+mowalon+jpease1020+hoffsmh+selfup@gmail.com>
2015-09-22 21:36:59 -0600

    added price column to items table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150922223853) do
+ActiveRecord::Schema.define(version: 20150923033526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20150922223853) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "dao"
+    t.decimal  "price"
   end
 
   add_index "items", ["category_id"], name: "index_items_on_category_id", using: :btree
