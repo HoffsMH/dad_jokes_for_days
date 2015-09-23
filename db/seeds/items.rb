ea1ef764313d87ea456007b7f3c33be4fc2a5eed
Alon Waisman, Justin Pease, Matthew Hecker and Regis Boudinot <pair+mowalon+jpease1020+hoffsmh+selfup@gmail.com>
2015-09-22 21:49:57 -0600

    updated fixtures and seeds to include price

--- db/seeds/items.rb
+++ db/seeds/items.rb
@@ -1,10 +1,10 @@
 category = Category.find_by_name("T-shirts")
-category.items.create(name: "Mens  T-shirt", description: "100% Cotton", image_url: "fake_url")
-category.items.create(name: "Womens T-shirt", description: "100% Cotton", image_url: "fake url")
+category.items.create(name: "Mens  T-shirt", description: "100% Cotton", image_url: "fake_url", price: 10.43)
+category.items.create(name: "Womens T-shirt", description: "100% Cotton", image_url: "fake url", price: 11.43)
 
 category = Category.find_by_name("HouseHold")
-category.items.create(name: "Mug", description: "100% Ceramic", image_url: "fake_url")
+category.items.create(name: "Mug", description: "100% Ceramic", image_url: "fake_url", price: 4.43)
 
 category = Category.find_by_name("Stationery")
-category.items.create(name: "Greeting Card", description: "100% Paper", image_url: "fake_url")
-category.items.create(name: "Notepad", description: "100% Paper", image_url: "fake_url")
+category.items.create(name: "Greeting Card", description: "100% Paper", image_url: "fake_url", price: 400.43)
+category.items.create(name: "Notepad", description: "100% Paper", image_url: "fake_url", price: 1.43)
