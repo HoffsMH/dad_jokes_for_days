3b30f8ce917bfb126d34279f0e55a027d5830953
Alon Waisman, Justin Pease, Matthew Hecker and Regis Boudinot <pair+mowalon+jpease1020+hoffsmh+selfup@gmail.com>
2015-09-22 20:49:51 -0600

    Add valid seeds for items

--- db/seeds/items.rb
+++ db/seeds/items.rb
@@ -1,10 +1,10 @@
 category = Category.find_by_name("T-shirts")
-category.items.create(name: "Mens  T-shirt", description: "100% Cotton")
-category.items.create(name: "Womens T-shirt", description: "100% Cotton")
+category.items.create(name: "Mens  T-shirt", description: "100% Cotton", image_url: "fake_url")
+category.items.create(name: "Womens T-shirt", description: "100% Cotton", image_url: "fake url")
 
 category = Category.find_by_name("HouseHold")
-category.items.create(name: "Mug", description: "100% Ceramic")
+category.items.create(name: "Mug", description: "100% Ceramic", image_url: "fake_url")
 
 category = Category.find_by_name("Stationery")
-category.items.create(name: "Greeting Card", description: "100% Paper")
-category.items.create(name: "Notepad", description: "100% Paper")
+category.items.create(name: "Greeting Card", description: "100% Paper", image_url: "fake_url")
+category.items.create(name: "Notepad", description: "100% Paper", image_url: "fake_url")
