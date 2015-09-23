42f845858f8fb1307498dad4447c98dff3d2c7bd
Alon Waisman, Justin Pease, Matthew Hecker and Regis Boudinot <pair+mowalon+jpease1020+hoffsmh+selfup@gmail.com>
2015-09-22 19:50:48 -0600

    starting to add session inside carts controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,5 +1,12 @@
 class CartsController < ApplicationController
   def create
+    if session[:cart]
+      session[:cart][params[:item_id]] += 1
+    else
+
+      session[:cart] = {params[:item_id] => 1}
+    end
+    @cart_items = session[:cart]
 
     redirect_to '/cart'
 
