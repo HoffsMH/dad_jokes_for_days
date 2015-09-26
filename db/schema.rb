bd1f86ca98191f1112d03aa6a0a53420ae2b7e94
Regis <boudinot.regis@yahoo.com>
2015-09-26 16:29:04 -0600

    Added orders table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150924031110) do
+ActiveRecord::Schema.define(version: 20150926222731) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,15 @@ ActiveRecord::Schema.define(version: 20150924031110) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.string   "status"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "orders", ["user_id"], name: "index_orders_on_user_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "user_name"
@@ -53,4 +62,5 @@ ActiveRecord::Schema.define(version: 20150924031110) do
   end
 
   add_foreign_key "items", "categories"
+  add_foreign_key "orders", "users"
 end
