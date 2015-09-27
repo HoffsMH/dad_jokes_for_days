6bea584761bb5a98a57f77c957a52c24b21d7d96
Alon Waisman, Justin Pease, Matthew Hecker and Regis Boudinot <pair+mowalon+jpease1020+hoffsmh+selfup@gmail.com>
2015-09-27 16:27:46 -0600

    updated dashboard page

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,13 +3,17 @@ class CartsController < ApplicationController
     session[:cart] ||= {}
     session[:cart][params[:item_id]] ||= 0
     session[:cart][params[:item_id]] += 1
-
+    
     flash[:notice] = 'Added ' + Item.find_by_dao(params[:item_id]).name
     redirect_to cart_path
   end
-
+  
   def show
-    @user_message = current_user.user_name || "Please log in before checking out"
+    if current_user
+      @user_message = current_user.user_name
+    else
+      @user_message = "Please Log in before checking out"
+    end
     @cart_items = if session[:cart] && !session[:cart].empty?
       session[:cart].map do |item_dao, quantity|
         CartItem.new(item_dao, quantity)
@@ -18,7 +22,7 @@ class CartsController < ApplicationController
     
     @cart_total = grand_total if @cart_items
   end
-
+  
   def update
     if new_quantity > 0
       session[:cart][params[:id]] = new_quantity
@@ -28,22 +32,22 @@ class CartsController < ApplicationController
       flash[:notice] = "Successfully removed <a href='/items/#{item.dao}'>#{item.name}</a> from your cart."
       session[:cart].delete(params[:id])
     end
-
+    
     redirect_to cart_path
   end
-
+  
   def destroy
     session.delete(:cart)
     flash[:notice] = "Cart deleted!"
     redirect_to cart_path
   end
-
+  
   private
-
+  
   def grand_total
     @cart_items.reduce(0){|sum, cart_item| sum += cart_item.total}
   end
-
+  
   def new_quantity
     if params[:commit] == "Update Quantity"
       params[:item][:quantity].to_i
@@ -51,5 +55,5 @@ class CartsController < ApplicationController
       0
     end
   end
-
+  
 end
