f5bfb330d911964daceb38fb804c5febee10cfaa
Alon Waisman, Justin Pease, Matthew Hecker and Regis Boudinot <pair+mowalon+jpease1020+hoffsmh+selfup@gmail.com>
2015-09-27 17:10:00 -0600

    Remove item works

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -15,36 +15,36 @@ class CartsController < ApplicationController
     flash[:notice] = 'Added ' + Item.find_by_dao(params[:item_id]).name
     redirect_to cart_path
   end
-  
+
   def show
     @order_items = OrderItem.where(id: session[:cart])
   end
-  
+
   def update
     if new_quantity > 0
       OrderItem.find(params[:order_item][:order_item_id])
       flash[:notice] = "Quantity updated"
     else
-      OrderItem.find(params[:order_item][:order_item_id]).delete
-      flash[:notice] = "Successfully removed <a href='/items/#{item.dao}'>#{item.name}</a> from your cart."
-      session[:cart].delete(params[:id])
+      order_item = OrderItem.find(params[:order_item][:order_item_id]).delete
+      session[:cart].delete(order_item.id)
+      flash[:notice] = "Successfully removed <a href=\"/items/#{order_item.item.dao}\">#{order_item.item.name}</a> from your cart."
     end
-    
+
     redirect_to cart_path
   end
-  
+
   def destroy
     session.delete(:cart)
     flash[:notice] = "Cart deleted!"
     redirect_to cart_path
   end
-  
+
   private
-  
+
   def grand_total
     @cart_items.reduce(0){|sum, cart_item| sum += cart_item.total}
   end
-  
+
   def new_quantity
     if params[:commit] == "Update Quantity"
       params[:order_item][:quantity].to_i
@@ -52,9 +52,6 @@ class CartsController < ApplicationController
       0
     end
   end
-<<<<<<< HEAD
-  
-=======
 
   def order_item_in_session
     # order_item_ids = session[:cart].delete("[]").split(",")
@@ -62,5 +59,4 @@ class CartsController < ApplicationController
     OrderItem.where(id: session[:cart], joke_id: session[:joke_id], item_id: item.id).first
   end
 
->>>>>>> master
 end
