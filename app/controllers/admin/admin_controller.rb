aa5acdf5b024d8467cf6c6b6b8c3ed8a282a4534
Alon Waisman, Justin Pease, Matthew Hecker and Regis Boudinot <pair+mowalon+jpease1020+hoffsmh+selfup@gmail.com>
2015-09-28 16:45:39 -0600

    closes #93

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -2,8 +2,8 @@ class Admin::AdminController < ApplicationController
   before_action :authorized?
 
   def authorized?
-    if !current_user.admin?
-      redirect_to 404
+    if current_user.nil? || !current_user.admin?
+      render plain: "404", status: 404
     end
   end
 end
