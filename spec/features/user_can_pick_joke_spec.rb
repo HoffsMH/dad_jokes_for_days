1c910026a216f958ec5ab9adb410288a47499a18
Alon Waisman, Justin Pease, Matthew Hecker and Regis Boudinot <pair+mowalon+jpease1020+hoffsmh+selfup@gmail.com>
2015-09-28 21:34:42 -0600

    added test for jokes
    not stubbing yet but atleast making sure there is a joke there

--- spec/features/user_can_pick_joke_spec.rb
+++ spec/features/user_can_pick_joke_spec.rb
@@ -5,19 +5,20 @@ feature "The home page" do
   before(:each) do
     visit "/"
   end
-
+  
   it "has a random joke option" do
     expect(page).to have_button("Pick a random joke for me!")
   end
-
+  
   it "leads to /items after selecting a joke" do
     click_button "Pick a random joke for me!"
     expect(current_path).to eq(items_path)
   end
-
-  xit "displays the joke on the page after selection" do
+  
+  it "displays the joke on the page after selection" do
+    Joke.all.stub(:sample).and_return(Joke.all.first)
     click_button "Pick a random joke for me!"
-    expect(page).to have_content("Current chosen Dad Joke: XXXXXX")
+    expect(page).to have_content(" ... ")
   end
-
+  
 end
