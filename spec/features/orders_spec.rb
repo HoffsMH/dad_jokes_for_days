4a73c548cd5a8206ee6dca854f8d5787f7bef5d9
Alon Waisman, Justin Pease, Matthew Hecker and Regis Boudinot <pair+mowalon+jpease1020+hoffsmh+selfup@gmail.com>
2015-09-28 20:11:30 -0600

    added test for updating and removing items from the cart

---
+++ spec/features/orders_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+feature "orders" do
+  
+  before(:each) do
+    visit "/"
+  end
+  context "when logged in" do
+    before(:each) do
+      visit "/"
+      click_link "Login"
+      fill_in "Email", with: "jeff@gmail.com"
+      fill_in "Password", with: "pass"
+      click_button("Login")
+    end
+    it "Redirects to Dashboard" do
+      visit "/"
+      click_link "All Products"
+      first(:button, "Add To Cart").click
+      expect(current_path).to eq('/cart')
+      click_link "Checkout"
+      click_button "Confirm"
+      expect(current_path).to eq('/dashboard')
+    end
+    
+    it "Creates the Order" do
+      visit "/"
+      click_link "All Products"
+      first(:button, "Add To Cart").click
+      expect(current_path).to eq('/cart')
+      click_link "Checkout"
+      click_button "Confirm"
+      expect(page).to have_content("Invoice #")
+    end
+  end
+end
---
+++ spec/features/update_cart_items_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+feature "update cart" do
+  fixtures :categories
+  fixtures :items
+  fixtures :users
+
+  context "Items are in the cart" do
+    before(:each) do
+      visit "/"
+      click_link("All Products")
+      first(:button, "Add To Cart").click
+      
+    end
+    it "can update quantity" do
+      fill_in "order_item[quantity]", with: "20"
+      click_button "Update Quantity"
+      expect(page).to have_selector("input[value='20']")
+    end
+    it "can delete item from cart" do
+      fill_in "order_item[quantity]", with: "20"
+      click_button "Remove"
+      expect(page).not_to have_button("Remove")
+    end
+  end
+  
+end
