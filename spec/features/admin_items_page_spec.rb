dc55f1cb8f0a48dedebc3fb4954d54e156bd6f66
Alon Waisman, Justin Pease, Matthew Hecker and Regis Boudinot <pair+mowalon+jpease1020+hoffsmh+selfup@gmail.com>
2015-09-29 17:46:01 -0600

    added test for admin page
    added test that make sure admin can access the page

--- spec/features/admin_items_page_spec.rb
+++ spec/features/admin_items_page_spec.rb
@@ -31,4 +31,23 @@ feature "admin items page" do
       expect(page.status_code).to eq(404)
     end
   end
+
+  context "as and authenticated admin" do
+    before(:each) do
+      visit "/"
+      click_link("Login")
+      fill_in("Email", with: "admin@admin.com")
+      fill_in("Password", with: "admin")
+      click_button "Login"
+      visit "/admin/items"
+    end
+    it "returns admin items page" do
+      expect(page).not_to have_content(404)
+      expect(page).to have_content("Admin Items")
+    end
+    it "404s" do
+      expect(page.status_code).to eq(200)
+    end
+  end
+
 end
