09f10b88b21a807303f48745e716d96950157c4c
Alon Waisman, Justin Pease, Matthew Hecker and Regis Boudinot <pair+mowalon+jpease1020+hoffsmh+selfup@gmail.com>
2015-09-28 22:56:23 -0600

    added test to make for item dao
    making sure that parameter version of item yields its dao

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -63,6 +63,11 @@ describe Item do
     item.valid?
     expect(item.dao).to eq('mugmug')
   end
+  it "paramaterizing yields dao" do
+    item.dao = nil
+    item.valid?
+    expect(item.to_param).to eq('mugmug')
+  end
 
   it "successfully adds an item to the database" do
     expect(item.name).to eq("mug mug")
