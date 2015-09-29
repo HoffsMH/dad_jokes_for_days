08cf9c9b0ac2706ad70dd0d6edd40e379fa594b4
Alon Waisman and Regis Boudinot <pair+mowalon+selfup@gmail.com>
2015-09-29 14:27:18 -0600

    Changed more apostrophes

--- spec/features/user_signup_functionality_spec.rb
+++ spec/features/user_signup_functionality_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-describe 'a new user signin up', type: :feature do
+describe "a new user signin up", type: :feature do
   fixtures :categories
   fixtures :items
   fixtures :users
@@ -12,10 +12,10 @@ describe 'a new user signin up', type: :feature do
     end
 
     it "can create a new account" do
-      fill_in('new_user[email]', with: 'Jeff@gmail.com')
-      fill_in('new_user[user_name]', with: 'Jeffy')
-      fill_in('new_user[password]', with: 'pass')
-      fill_in('new_user[password_confirmation]', with: 'pass')
+      fill_in("new_user[email]", with: "Jeff@gmail.com")
+      fill_in("new_user[user_name]", with: "Jeffy")
+      fill_in("new_user[password]", with: "pass")
+      fill_in("new_user[password_confirmation]", with: "pass")
       click_button "Create"
       expect(current_path).to eq(dashboard_path)
       expect(page).to have_content("Username:")
@@ -23,48 +23,48 @@ describe 'a new user signin up', type: :feature do
     end
 
     it "can not create an account without a username" do
-      fill_in('new_user[email]', with: 'Jeff@gmail.com')
-      fill_in('new_user[password]', with: 'pass')
-      fill_in('new_user[password_confirmation]', with: 'pass')
+      fill_in("new_user[email]", with: "Jeff@gmail.com")
+      fill_in("new_user[password]", with: "pass")
+      fill_in("new_user[password_confirmation]", with: "pass")
       click_button "Create"
-      expect(current_path).to eq('/register')
+      expect(current_path).to eq("/register")
       expect(page).to have_content("User name can't be blank")
     end
 
     it "can not create an account without an email" do
-      fill_in('new_user[user_name]', with: 'Jeffy')
-      fill_in('new_user[password]', with: 'pass')
-      fill_in('new_user[password_confirmation]', with: 'pass')
+      fill_in("new_user[user_name]", with: "Jeffy")
+      fill_in("new_user[password]", with: "pass")
+      fill_in("new_user[password_confirmation]", with: "pass")
       click_button "Create"
-      expect(current_path).to eq('/register')
+      expect(current_path).to eq("/register")
       expect(page).to have_content("Email can't be blank")
     end
 
     it "can not create an account with an invalid email address" do
-      fill_in('new_user[password]', with: 'pass')
-      fill_in('new_user[email]', with: 'Jeffgmailcom')
-      fill_in('new_user[user_name]', with: 'Jeffy')
-      fill_in('new_user[password_confirmation]', with: 'pass')
+      fill_in("new_user[password]", with: "pass")
+      fill_in("new_user[email]", with: "Jeffgmailcom")
+      fill_in("new_user[user_name]", with: "Jeffy")
+      fill_in("new_user[password_confirmation]", with: "pass")
       click_button "Create"
-      expect(current_path).to eq('/register')
+      expect(current_path).to eq("/register")
       expect(page).to have_content("Email is invalid")
     end
 
     it "can not create an account without a password" do
-      fill_in('new_user[email]', with: 'Jeff@gmail.com')
-      fill_in('new_user[user_name]', with: 'Jeffy')
-      fill_in('new_user[password_confirmation]', with: 'pass')
+      fill_in("new_user[email]", with: "Jeff@gmail.com")
+      fill_in("new_user[user_name]", with: "Jeffy")
+      fill_in("new_user[password_confirmation]", with: "pass")
       click_button "Create"
-      expect(current_path).to eq('/register')
+      expect(current_path).to eq("/register")
       expect(page).to have_content("Password can't be blank")
     end
 
     it "can not create an account without a password confirmation" do
-      fill_in('new_user[email]', with: 'Jeff@gmail.com')
-      fill_in('new_user[user_name]', with: 'Jeffy')
-      fill_in('new_user[password]', with: 'pass')
+      fill_in("new_user[email]", with: "Jeff@gmail.com")
+      fill_in("new_user[user_name]", with: "Jeffy")
+      fill_in("new_user[password]", with: "pass")
       click_button "Create"
-      expect(current_path).to eq('/register')
+      expect(current_path).to eq("/register")
       expect(page).to have_content("Password confirmation doesn't match Password")
     end
   end
@@ -73,10 +73,10 @@ describe 'a new user signin up', type: :feature do
     before(:each) do
       visit "/"
       click_link("Create Account")
-      fill_in('new_user[email]', with: 'Jeff@gmail.com')
-      fill_in('new_user[user_name]', with: 'Jeffy')
-      fill_in('new_user[password]', with: 'pass')
-      fill_in('new_user[password_confirmation]', with: 'pass')
+      fill_in("new_user[email]", with: "Jeff@gmail.com")
+      fill_in("new_user[user_name]", with: "Jeffy")
+      fill_in("new_user[password]", with: "pass")
+      fill_in("new_user[password_confirmation]", with: "pass")
       click_button "Create"
     end
 
@@ -84,10 +84,10 @@ describe 'a new user signin up', type: :feature do
       click_link("Logout")
       visit "/"
       click_link("Create Account")
-      fill_in('new_user[email]', with: 'Jeff@gmail.com')
-      fill_in('new_user[user_name]', with: 'J')
-      fill_in('new_user[password]', with: 'pa')
-      fill_in('new_user[password_confirmation]', with: 'pa')
+      fill_in("new_user[email]", with: "Jeff@gmail.com")
+      fill_in("new_user[user_name]", with: "J")
+      fill_in("new_user[password]", with: "pa")
+      fill_in("new_user[password_confirmation]", with: "pa")
       click_button "Create"
       expect(page).to have_content("Email has already been taken")
     end
@@ -96,12 +96,12 @@ describe 'a new user signin up', type: :feature do
     before(:each) do
       visit "/"
       click_link("Login")
-      fill_in("Email", with: 'jeff@gmail.com')
-      fill_in("Password", with: 'pass')
+      fill_in("Email", with: "jeff@gmail.com")
+      fill_in("Password", with: "pass")
       click_button "Login"
     end
     it "can not create an account" do
-      visit '/register'
+      visit "/register"
       expect(page).to have_content("Please Log out before making a new Account")
     end
   end
