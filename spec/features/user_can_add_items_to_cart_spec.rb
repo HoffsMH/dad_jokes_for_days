d74260be951190b38d500ab32b3568fc5852ac5a
Alon Waisman, Justin Pease, Matthew Hecker and Regis Boudinot <pair+mowalon+jpease1020+hoffsmh+selfup@gmail.com>
2015-09-28 22:08:34 -0600

    added test to make sure cart items dont duplicate
    adding the same item/joke doesnt result in two cart items

--- spec/features/user_can_add_items_to_cart_spec.rb
+++ spec/features/user_can_add_items_to_cart_spec.rb
@@ -4,16 +4,16 @@ feature "cart" do
   fixtures :categories
   fixtures :items
   fixtures :users
-
+  
   before(:each) do
     visit "/"
   end
-
+  
   it "can visit the cart page" do
     click_link("Cart")
     expect(current_path).to eq('/cart')
   end
-
+  
   it "can add items" do
     click_link("All Products")
     first(:button, "Add To Cart").click
@@ -21,7 +21,7 @@ feature "cart" do
     expect(page).to have_content("Item A")
     expect(page).to have_content("$11.11")
   end
-
+  
   context "user is logged in" do
     before(:each) {
       visit '/login'
@@ -29,28 +29,28 @@ feature "cart" do
       fill_in "Password", with: "pass"
       click_button("Login")
     }
-
+    
     it "can checkout to buy items" do
       click_link("All Products")
       first(:button, "Add To Cart").click
       click_link("Checkout")
-
+      
       expect(current_path).to eq("/checkout")
     end
   end
-
+  
   context "user is not logged in" do
-
+    
     it "must log in before checking out" do
       visit '/'
       click_link("All Products")
       first(:button, "Add To Cart").click
-
+      
       expect(page).to have_content("Please login before checking out.")
       expect(page).to have_content("Login")
       expect(page).not_to have_content("Checkout")
     end
-
+    
     it "can log in and proceed to checkout" do
       visit '/'
       click_link("All Products")
@@ -64,8 +64,20 @@ feature "cart" do
       click_button("Login")
       expect(current_path).to eq("/cart")
     end
+    context "we have already an item in our cart" do
+      before(:each) do
+        visit '/'
+        click_link("All Products")
+        first(:button, "Add To Cart").click
+      end
+      
+      it "lets you add the same item/joke twice" do
+        visit '/'
+        click_link("All Products")
+        first(:button, "Add To Cart").click
+        expect(page).to have_selector('.cart-row', count: 1)
+      end
+    end
   end
-
-  # xit "user adds multiple items to cart and tries to buy"
-
+  
 end
