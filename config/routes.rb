d30d0d25b70b3703d3ddeb15a4ad3d3a0909d583
Alon Waisman, Justin Pease, Matthew Hecker and Regis Boudinot <pair+mowalon+jpease1020+hoffsmh+selfup@gmail.com>
2015-09-28 18:18:02 -0600

    Added update functionality for users
    Updated user routes - modified show page to allow editing of user info - created update method in controller and update user params for that method
    
    Now user can update their information from the user dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get '/register' => 'users#new'
   post '/register' => 'users#create'
   get '/dashboard' => 'users#show'
+  patch '/dashboard' => 'users#update'
   get '/jokes' => 'jokes#index'
 
   resources :categories, only: [:index]
