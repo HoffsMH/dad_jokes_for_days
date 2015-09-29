a9e744eaafd9c503a57034e8c35bca6bd9ca71ac
Alon Waisman and Regis Boudinot <pair+mowalon+selfup@gmail.com>
2015-09-28 18:39:16 -0600

    Test user info update
    Added test for user updating info. Confirm the info can be updated and then used to login. Also changed 'Update user' button to 'Update Info' on dashboard page.

---
+++ spec/features/user_can_edit_account_info_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+feature "Account info update" do
+  fixtures :users
+
+  before(:each) do
+    visit "/login"
+    fill_in "Email", with: "jeff@gmail.com"
+    fill_in "Password", with: "pass"
+    click_button("Login")
+  end
+
+  it "user can edit account info" do
+    fill_in("user[user_name]", with: "newname")
+    fill_in("user[email]", with: "newemail@new.com")
+    click_button("Update Info")
+
+    expect(page).to have_content("You have successfully updated your information!")
+
+    click_link("Logout")
+    click_link("Login")
+    fill_in "Email", with: "newemail@new.com"
+    fill_in "Password", with: "pass"
+    click_button("Login")
+
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to have_content("The Dad welcomes you")
+  end
+
+end
