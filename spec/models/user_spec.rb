89b5208b2d550c85880930a1d42e1ebfd3a201d4
Alon Waisman, Justin Pease and Regis Boudinot <pair+mowalon+jpease1020+selfup@gmail.com>
2015-09-29 21:48:39 -0600

    tests the user model
    validates that a user is only valid with a unique email, a user name and a confirmed password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,31 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
+  let(:user) do
+    User.new(email: "joe@example.com", user_name: "joe", password: "pass")
+end
+
+  it 'is valid' do
+    expect(user).to be_valid
+  end
+
+  it "is invalid without a user name" do
+    user.user_name = nil
+    expect(user).not_to be_valid
+  end
+
+  it "is invalid with out an email" do
+    user.email = nil
+    expect(user).not_to be_valid
+  end
+
+  it "is invalid with out unique email" do
+    user.save
+    user2 = User.create(email: "joe@example.com", user_name: "joe", password: "pass")
+    expect(user2).not_to be_valid
+  end
+
+  xit "is invalid with out a password confirmation" do
+
+  end
 end
