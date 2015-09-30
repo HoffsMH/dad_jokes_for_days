d45e3411b4690009ee4aa872c04715aededd0bd7
Alon Waisman, Justin Pease and Regis Boudinot <pair+mowalon+jpease1020+selfup@gmail.com>
2015-09-29 22:54:56 -0600

    commit to merge new master

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
 
   def create
-    order = Order.create(status: "complete", user_id: session[:user])
+    order = Order.create(status: "ordered", user_id: session[:user])
     order.order_items << OrderItem.where(id: session[:cart])
     session.delete(:cart)
     flash[:notice] = "Thank you for your order!"
