057217b70ad95468c34881f8d8f5fc52fda2ae6d
Alon Waisman, Justin Pease and Regis Boudinot <pair+mowalon+jpease1020+selfup@gmail.com>
2015-09-29 20:32:59 -0600

    creates table to display all orders for admin to view
    Added columns for order id, order created at time, order user/purchaser, order quantity, total revenue and status.
    Adds subtotal to order_items table

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,15 @@
 class Order < ActiveRecord::Base
   belongs_to :user
   has_many :order_items
-  
+
   validates :user_id, :status, presence: true
+
+  def quantity
+    order_items.sum(:quantity)
+  end
+
+  def total
+    order_items.sum(:subtotal)
+  end
+
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,9 +2,15 @@ class OrderItem < ActiveRecord::Base
   belongs_to :item
   belongs_to :joke
   belongs_to :order
+  before_validation :fill_subtotal
 
   validates :item_id, :joke_id, :quantity, presence: true
   def total
     quantity * item.price
   end
+
+  def fill_subtotal
+    self.subtotal = total
+  end
+
 end
