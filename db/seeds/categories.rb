17aa5746ec31bddb2b9cebd1a3c8cd9cb505af64
Alon Waisman, Justin Pease, Matthew Hecker and Regis Boudinot <pair+mowalon+jpease1020+hoffsmh+selfup@gmail.com>
2015-09-29 18:35:04 -0600

    updated the rest of seeds

--- db/seeds/categories.rb
+++ db/seeds/categories.rb
@@ -1,3 +1,3 @@
-Category.create(name: "T-shirts", image_url: "https://s3-us-west-2.amazonaws.com/dadjokesfordays/images/mens_t-shirt--600x600.jpg")
-Category.create(name: "Household Items", image_url: "https://s3-us-west-2.amazonaws.com/dadjokesfordays/images/mug_with_MUG_joke--600x600.jpg")
-Category.create(name: "Stationery", image_url: "https://s3-us-west-2.amazonaws.com/dadjokesfordays/images/notepad--600x600.jpg")
+puts Category.create(name: "T-shirts", image_url: "https://s3-us-west-2.amazonaws.com/dadjokesfordays/images/mens_t-shirt--600x600.jpg")
+puts Category.create(name: "Household Items", image_url: "https://s3-us-west-2.amazonaws.com/dadjokesfordays/images/mug_with_MUG_joke--600x600.jpg")
+puts Category.create(name: "Stationery", image_url: "https://s3-us-west-2.amazonaws.com/dadjokesfordays/images/notepad--600x600.jpg")
